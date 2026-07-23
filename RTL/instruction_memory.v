65466d37c49cbc40085c1a9c7791a9b0b8c90a76
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-07-23 14:38:36 +0530

    Update instruction_memory.v

--- RTL/instruction_memory.v
+++ RTL/instruction_memory.v
@@ -1,8 +1,9 @@
 module instruction_memory(A,RD);
+
 input [31:0] A;
 output [31:0] RD;
 
-reg [31:0] mem [1023:0];
+reg [31:0] mem [1023:0];   
 
 assign RD = mem[A[31:2]];
 
@@ -10,6 +11,5 @@ initial begin
     mem[0] = 32'hFFC4A303;
     mem[1] = 32'h00832383;
 end
-
 
 endmodule
